b9c9ca2b8b35df857800386dd54bb3b5a0727812
kadamdipti <48252470+kadamdipti@users.noreply.github.com>
2020-05-28 20:29:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.description');
+var clouds = document.querySelector('.clouds');
+var humi = document.querySelector('.humidity');
+
+
+var button = document.querySelector('.submit');
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+button.addEventListener('click', function(name) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=50a7aa80fa492fa92e874d23ad061374&units=metric')
+        .then(response => response.json())
+        .then(data => {
+            var tempValue = data['main']['temp'];
+            var nameValue = data['name'];
+            var descValue = data['weather'][0]['description'];
+            var humidityValue = data['main']['humidity'];
+
+
+            main.innerHTML = nameValue;
+            desc.innerHTML = "Description - " + descValue;
+            temp.innerHTML = "Temp - " + tempValue;
+            humi.innerHTML = "humidity-" + humidityValue;
+
+            input.value = "";
+
+        })
+
+    .catch(err => alert("Wrong city name!"));
+})
